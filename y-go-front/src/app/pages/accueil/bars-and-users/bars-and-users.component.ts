dc8a10ab36d048bbab8a312650b67a38340a4ba7
Dessauw Hugo <dessauw.hugo@gmail.com>
2023-11-21 20:12:30 +0100

    Back : Configure 1/2  create-user-dto, Front: AppAccueil template

---
+++ y-go-front/src/app/pages/accueil/bars-and-users/bars-and-users.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: `app-us-users`,
+  templateUrl: `./bars-and-users.component.html`,
+  styleUrls: [`./bars-and-users.component.scss`],
+})
+export class BarsAndUsersComponent {}
