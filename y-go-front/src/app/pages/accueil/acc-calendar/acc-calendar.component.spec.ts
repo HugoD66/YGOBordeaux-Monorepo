c0d1b1392bf4e936a721cbb1b3fe4943ec9b5f15
HugoD66 <dessauw.hugo@gmail.com>
2024-01-17 12:00:58 +0100

    WIP responsive, add calendar pictures

---
+++ y-go-front/src/app/pages/accueil/acc-calendar/acc-calendar.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AccCalendarComponent } from './acc-calendar.component';
+
+describe('AccCalendarComponent', () => {
+  let component: AccCalendarComponent;
+  let fixture: ComponentFixture<AccCalendarComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AccCalendarComponent]
+    });
+    fixture = TestBed.createComponent(AccCalendarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ y-go-front/src/app/pages/accueil/acc-calendar/acc-calendar.component.ts
@@ -0,0 +1,28 @@
+import { Component, ChangeDetectorRef } from "@angular/core"
+
+@Component({
+  selector: `app-acc-calendar`,
+  templateUrl: `./acc-calendar.component.html`,
+  styleUrls: [`./acc-calendar.component.scss`],
+})
+export class AccCalendarComponent {
+  calendarPictureList = [`9.png`, `8.png`, `7.png`, `6.png`, `5.png`, `4.png`, `3.png`, `2.png`, `1.png`]
+  currentImageIndex = 0
+  animateClass = false
+
+  constructor(private cdr: ChangeDetectorRef) {
+    setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.calendarPictureList.length
+      this.triggerAnimation()
+    }, 1000)
+  }
+
+  triggerAnimation() {
+    this.animateClass = true
+    this.cdr.detectChanges() // Force la détection de changements
+    setTimeout(() => {
+      this.animateClass = false
+      this.cdr.detectChanges() // Force la détection de changements
+    }, 300)
+  }
+}
